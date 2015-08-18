09b33cbaff642eae701b77576d8435b34807e864
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-08-18 10:00:09 -0600

    tag version

--- cyvcf2/__init__.py
+++ cyvcf2/__init__.py
@@ -1,3 +1,5 @@
 #
 from .cyvcf2 import VCF, Variant, Writer
 Reader = VCFReader = VCF
+
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
+import os
 import glob
+
+def get_version():
+    """Get the version info from the mpld3 package without importing it"""
+    import ast
+
+    with open(os.path.join("cyvcf2", "__init__.py"), "r") as init_file:
+      module = ast.parse(init_file.read())
+
+    version = (ast.literal_eval(node.value) for node in ast.walk(module)
+         if isinstance(node, ast.Assign)
+         and node.targets[0].id == "__version__")
+    try:
+      return next(version)
+    except StopIteration:
+          raise ValueError("version could not be located")
+
 
 setup(
     name="cyvcf2",
+    version=get_version(),
     ext_modules=cythonize([
         Extension("cyvcf2.cyvcf2", ["cyvcf2/cyvcf2.pyx"], libraries=["hts"],
             include_dirs=["cyvcf2"], extra_objects=["cyvcf2/helpers.c",

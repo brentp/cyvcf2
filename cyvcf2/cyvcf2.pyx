f590ae0f270215c582a2002aeb0f94397a896a28
Brent Pedersen <bpederse@gmail.com>
2019-02-19 11:13:20 -0700

    add alternative genotypes array impl

--- cyvcf2/cyvcf2.pyx
+++ cyvcf2/cyvcf2.pyx
@@ -963,6 +963,35 @@ cdef class Genotypes(object):
             result.append((v >> 1) - 1)
         return result
 
+    def array(Genotypes self):
+        """
+        array returns an int16 numpy array  of shape n_samples, (ploidy + 1).
+        The last column indicates phased (1 is phased, 0 is unphased).
+        The other columns indicate the alleles, e.g. [0, 1, 1] is 0|1.
+        """
+        cdef np.int16_t* to_return = <np.int16_t *>stdlib.malloc(sizeof(np.int16_t)
+                                                   * self.n_samples
+                                                   * (self.ploidy+1))
+
+        cdef int ind
+        cdef int allele
+        cdef int p = self.ploidy + 1
+
+        for ind in range(self.n_samples):
+            for allele in range(self.ploidy):
+                to_return[ind * p + allele] = (self._raw[ind * self.ploidy + allele] >> 1) - 1
+            to_return[ind * p + self.ploidy] = (self._raw[ind * self.ploidy + 1] & 1) == 1
+
+        cdef np.npy_intp shape[2]
+        shape[0] = self.n_samples
+        shape[1] = self.ploidy + 1
+        return np.PyArray_SimpleNewFromData(
+            2,
+            shape,
+            np.NPY_INT16,
+            to_return
+        )
+
     def alleles_array(self):
         cdef int ind
         cdef int allele

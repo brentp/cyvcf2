11eacd6cc9ca2203a54fbbe52618e5753a9d622c
Brent Pedersen <bpederse@gmail.com>
2021-04-15 18:17:06 +0200

    bumping to test ci
    and fixing travis yml

--- CHANGES.md
+++ CHANGES.md
@@ -1,2 +1,5 @@
+# 0.3.8
++ just bumping for CI+wheels
+
 # 0.30.7
 + fix gt_types for phased variants (see: #198)
--- cyvcf2/__init__.py
+++ cyvcf2/__init__.py
@@ -2,4 +2,4 @@ from .cyvcf2 import (VCF, Variant, Writer, r_ as r_unphased, par_relatedness,
                      par_het)
 Reader = VCFReader = VCF
 
-__version__ = "0.30.7"
+__version__ = "0.30.8"

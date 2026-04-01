b3718e3209654a9f0bf8e8a828c532d1b025b66e
SamuelNicaise <ichtyornis@gmail.com>
2026-04-01 17:03:46 +0200

    Add CYVCF2_HTSLIB_LOG_LEVEL environment variable and set_htslib_log_level() method and related tests

--- cyvcf2/tests/test_cli.py
+++ cyvcf2/tests/test_cli.py
@@ -4,9 +4,11 @@ import os.path
 from click.testing import CliRunner
 
 from cyvcf2.cli import cyvcf2
+from cyvcf2.cyvcf2 import set_htslib_log_level
 
 HERE = os.path.dirname(__file__)
 VCF_PATH = os.path.join(HERE, "test.vcf.gz")
+VCF_WITH_WARNINGS_PATH = os.path.join(HERE, "test.warnings.vcf.gz")
 
 
 #Sanity check for cli
@@ -14,3 +16,30 @@ def test_version():
     runner = CliRunner()
     result = runner.invoke(cyvcf2, [VCF_PATH])
     assert result.exit_code == 0
+
+
+def test_htslib_log_level(capfd):
+    runner = CliRunner()
+
+    for log_level in [4, 5]:
+        set_htslib_log_level(log_level)
+        runner = CliRunner()
+        _ = runner.invoke(cyvcf2, [VCF_WITH_WARNINGS_PATH])
+        captured = capfd.readouterr()
+        assert "[W::vcf_parse_format" in captured.err
+        assert "[I::hts_idx_check_local" in captured.err
+
+    set_htslib_log_level(3)
+    runner = CliRunner()
+    _ = runner.invoke(cyvcf2, [VCF_WITH_WARNINGS_PATH])
+    captured = capfd.readouterr()
+    assert "[W::vcf_parse_format" in captured.err
+    assert "[I::hts_idx_check_local" not in captured.err
+
+    for log_level in [0, 1, 2]:
+        set_htslib_log_level(log_level)
+        runner = CliRunner()
+        _ = runner.invoke(cyvcf2, [VCF_WITH_WARNINGS_PATH])
+        captured = capfd.readouterr()
+        assert "[W::vcf_parse_format" not in captured.err
+        assert "[I::hts_idx_check_local" not in captured.err
